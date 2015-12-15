12abfcbdafc09f79b881acc17abd0168d1ebba2f
gq6pras <gq6pras@L7GCC5CG44036VV.ent.rt.csaa.com>
2015-12-15 11:41:08 -0700

    Hacker Rank Caesar Cipher Java Solution

--- HackerRank/src/main/java/com/gprasad/hackerrank/algo/Implementation/CaesarCipher.java
+++ HackerRank/src/main/java/com/gprasad/hackerrank/algo/Implementation/CaesarCipher.java
@@ -1,45 +1,45 @@
 /*
-Problem Statement
+ Problem Statement
 
-Julius Caesar protected his confidential information by encrypting it in a cipher. Caesar's cipher rotated every letter in a string by a fixed number, K, making it unreadable by his enemies. Given a string, S, and a number, K, encrypt S and print the resulting string.
+ Julius Caesar protected his confidential information by encrypting it in a cipher. Caesar's cipher rotated every letter in a string by a fixed number, K, making it unreadable by his enemies. Given a string, S, and a number, K, encrypt S and print the resulting string.
 
-Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
+ Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
 
-Input Format
+ Input Format
 
-The first line contains an integer, N, which is the length of the unencrypted string.
-The second line contains the unencrypted string, S.
-The third line contains the integer encryption key, K, which is the number of letters to rotate.
+ The first line contains an integer, N, which is the length of the unencrypted string.
+ The second line contains the unencrypted string, S.
+ The third line contains the integer encryption key, K, which is the number of letters to rotate.
 
-Constraints
-1≤N≤100
-0≤K≤100
-S is a valid ASCII string and doesn't contain any spaces.
+ Constraints
+ 1≤N≤100
+ 0≤K≤100
+ S is a valid ASCII string and doesn't contain any spaces.
 
-Output Format
+ Output Format
 
-For each test case, print the encoded string.
+ For each test case, print the encoded string.
 
-Sample Input
+ Sample Input
 
-11
-middle-Outz
-2
+ 11
+ middle-Outz
+ 2
 
-Sample Output
+ Sample Output
 
-okffng-Qwvb
+ okffng-Qwvb
 
-Explanation
+ Explanation
 
-Each unencrypted letter is replaced with the letter occurring K spaces after it when listed alphabetically. Think of the alphabet as being both case-sensitive and circular; if K rotates past the end of the alphabet, it loops back to the beginning (i.e.: the letter after z is a, and the letter after Z is A).
+ Each unencrypted letter is replaced with the letter occurring K spaces after it when listed alphabetically. Think of the alphabet as being both case-sensitive and circular; if K rotates past the end of the alphabet, it loops back to the beginning (i.e.: the letter after z is a, and the letter after Z is A).
 
-Selected Examples:
-m (ASCII 109) becomes o (ASCII 111).
-i (ASCII 105) becomes k (ASCII 107).
-− remains the same, as symbols are not encoded.
-O (ASCII 79) becomes Q (ASCII 81).
-z (ASCII 122) becomes b (ASCII 98); because z is the last letter of the alphabet, a (ASCII 97) is the next letter after it in lower-case rotation.
+ Selected Examples:
+ m (ASCII 109) becomes o (ASCII 111).
+ i (ASCII 105) becomes k (ASCII 107).
+ − remains the same, as symbols are not encoded.
+ O (ASCII 79) becomes Q (ASCII 81).
+ z (ASCII 122) becomes b (ASCII 98); because z is the last letter of the alphabet, a (ASCII 97) is the next letter after it in lower-case rotation.
 
  */
 package com.gprasad.hackerrank.algo.Implementation;
@@ -51,10 +51,37 @@ import java.util.Scanner;
  * @author gq6pras
  */
 public class CaesarCipher {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         String s = in.next();
         int k = in.nextInt();
+        System.out.println(rotateChar(n, s, k));
+    }
+
+    private static String rotateChar(int length, String givenStr, int shift) {
+        String rotateStr = "";
+        for (int i = 0; i < length; i++) {
+            char c = givenStr.charAt(i);
+            char shiftC = (char) (c + shift);
+            if (c >= 'A' && c <= 'Z') {
+                if (shiftC > 'Z') {
+                    rotateStr += (char) (c - (26 - shift));
+                } else {
+                    rotateStr += (char) (c + shift);
+                }
+            } else if (c >= 'a' && c <= 'z') {
+                if (shiftC > 'z') {
+                    rotateStr += (char) (c - (26 - shift));
+                } else {
+                    rotateStr += (char) (c + shift);
+                }
+            } else {
+                rotateStr += c;
+            }
+
+        }
+        return rotateStr;
     }
 }
