93010c012c98dada017928d79484fbb91fd2b360
gauravprasad <gq6pras@L7GCC5CG44036VV.ent.rt.csaa.com>
2016-02-04 15:56:44 -0700

    Pairs

--- HackerRank/src/main/java/com/gprasad/hackerrank/algo/strings/GameOfThronesOne.java
+++ HackerRank/src/main/java/com/gprasad/hackerrank/algo/strings/GameOfThronesOne.java
@@ -14,12 +14,29 @@ import java.util.Scanner;
 public class GameOfThronesOne {
 
     public static void main(String[] args) {
-        Scanner myScan = new Scanner(System.in);
-        String inputString = myScan.nextLine();
+        Scanner input = new Scanner(System.in);
+        String inputString = input.next();
+        System.out.println(ifAnagramPlaindrom(inputString) ? "YES" : "NO");
 
-        String ans=null;
-        // Assign ans a value of YES or NO, depending on whether or not inputString satisfies the required condition
-        System.out.println(ans);
-        myScan.close();
+    }
+
+    public static boolean ifAnagramPlaindrom(String input) {
+        boolean isPalindrom = true;
+        int[] charCount = new int[26];
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            charCount[c - 'a']++;
+        }
+        int charOddOccurence = 0;
+        for (int i = 0; i < 26; i++) {
+            if (charOddOccurence > 1) {
+                isPalindrom = false;
+                break;
+            }
+            if (charCount[i] % 2 == 1) {
+                charOddOccurence++;
+            }
+        }
+        return isPalindrom;
     }
 }
