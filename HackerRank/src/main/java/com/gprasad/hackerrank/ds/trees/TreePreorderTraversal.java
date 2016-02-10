76c37d5101da1eb8917c2fc1f8d82bae27f70eaf
Gaurav Prasad <gaurav.prasad@live.com>
2016-02-10 06:39:53 -0700

    Tree: Preorder Traversal

--- HackerRank/src/main/java/com/gprasad/hackerrank/ds/trees/TreePreorderTraversal.java
+++ HackerRank/src/main/java/com/gprasad/hackerrank/ds/trees/TreePreorderTraversal.java
@@ -1,7 +1,33 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+Tree: Preorder Traversal (Hackerrank Java solution)
+
+ You are given a pointer to the root of a binary tree; print the values in preorder traversal.
+
+You only have to complete the function.
+
+Input Format
+You are given a function,
+
+void Preorder(node *root) {
+
+}
+
+Output Format
+Print the values on a single line separated by space.
+
+Sample Input
+
+     3
+   /   \
+  5     2
+ / \    /
+1   4  6
+
+Sample Output
+
+3 5 1 4 2 6
+
  */
 package com.gprasad.hackerrank.ds.trees;
 
@@ -15,11 +41,11 @@ public class TreePreorderTraversal {
 
     }
 
-    private static void preorder(Node root) {
+    private static void Preorder(Node root) {
         if (root != null) {
-            System.out.println(root.data);
-            preorder(root.left);
-            preorder(root.right);
+            System.out.print(root.data + " ");
+            Preorder(root.left);
+            Preorder(root.right);
         }
     }
 }
