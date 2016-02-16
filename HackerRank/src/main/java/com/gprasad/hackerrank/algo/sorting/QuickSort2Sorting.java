0eee0654a5414a0b166e3a5c4149c491b83d9016
Gaurav Prasad <gaurav.prasad@live.com>
2016-02-15 22:49:07 -0700

    Quicksort 2 - Sorting (Hacker rank Java solution)

--- HackerRank/src/main/java/com/gprasad/hackerrank/algo/sorting/QuickSort2Sorting.java
+++ HackerRank/src/main/java/com/gprasad/hackerrank/algo/sorting/QuickSort2Sorting.java
@@ -1,7 +1,64 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 
+Quicksort 2 - Sorting (Hacker rank Java solution)
+
+
+In the previous challenge, you wrote a partition method to split an array into two sub-arrays, one containing smaller elements and one containing larger elements than a given number. This means you 'sorted' half the array with respect to the other half. Can you repeatedly use partition to sort an entire array?
+
+Guideline
+In Insertion Sort, you simply went through each element in order and inserted it into a sorted sub-array. In this challenge, you cannot focus on one element at a time, but instead must deal with whole sub-arrays, with a strategy known as "divide and conquer".
+
+When partition is called on an array, two parts of the array get 'sorted' with respect to each other. If partition is then called on each sub-array, the array will now be split into four parts. This process can be repeated until the sub-arrays are small. Notice that when partition is called on just one numbers, they end up being sorted.
+
+Can you repeatedly call partition so that the entire array ends up sorted?
+
+Print Sub-Arrays
+In this challenge, print your array every time your partitioning method finishes, i.e. whenever two subarrays, along with the pivot, is merged together.
+
+    The first element in a sub-array should be used as a pivot.
+    Partition the left side before partitioning the right side.
+    The pivot should be placed between sub-arrays while merging them.
+    Array of length 11 or less will be considered sorted, and there is no need to sort or to print them.
+
+Note
+Please maintain the original order of the elements in the left and right partitions while partitioning around a pivot element.
+
+For example: Partition about the first element for the array A[]={5, 8, 1, 3, 7, 9, 2} will be {1, 3, 2, 5, 8, 7, 9}
+
+Input Format
+There will be two lines of input:
+
+    nn - the size of the array
+    arar - the n numbers of the array
+
+Output Format
+Print every partitioned sub-array on a new line.
+
+Constraints
+1≤n≤10001≤n≤1000
+−1000≤x≤1000,x∈ar−1000≤x≤1000,x∈ar
+Each number is unique.
+
+Sample Input
+
+7
+5 8 1 3 7 9 2
+
+Sample Output
+
+2 3
+1 2 3
+7 8 9
+1 2 3 5 7 8 9
+
+Explanation
+This is a diagram of Quicksort operating on the sample array:
+
+quick-sort diagram
+
+Task
+The method quickSort takes in a parameter, arar, an unsorted array. Use the Quicksort algorithm to sort the entire array.
+
  */
 package com.gprasad.hackerrank.algo.sorting;
 
@@ -13,12 +70,16 @@ import java.util.Scanner;
  */
 public class QuickSort2Sorting {
 
-    static void quickSort(int[] ar) {
-        sort(0, ar.length - 1, ar);
-        printArray(ar);
+    static void quickSort(int[] ar, int start, int end) {
+        if (start < end) {
+            int pivotIndex = getPartition(ar, start, end);
+            quickSort(ar, start, pivotIndex - 1);
+            quickSort(ar, pivotIndex + 1, end);
+            printBetweenArr(ar,start,end);
+        }
     }
 
-    static void printArray(int[] ar) {
+    public static void printArray(int[] ar) {
         for (int n : ar) {
             System.out.print(n + " ");
         }
@@ -32,43 +93,30 @@ public class QuickSort2Sorting {
         for (int i = 0; i < n; i++) {
             ar[i] = in.nextInt();
         }
-        quickSort(ar);
+        quickSort(ar, 0, n - 1);
     }
 
-    private static void sort(int lower, int higher, int[] arr) {
+    private static int getPartition(int[] ar, int start, int end) {
+        int pivotIndex = start;
+        int pivotElement = ar[end];
+        for(int i=start;i<end;i++) {
+            if (ar[i] <= pivotElement) {
+                int temp = ar[i];
+                ar[i] = ar[pivotIndex];
+                ar[pivotIndex] = temp;
+                pivotIndex++;
+            }
+            
+        }
+            ar[end] = ar[pivotIndex];
+            ar[pivotIndex] = pivotElement;
+        return pivotIndex;
+    }
 
-        int i = lower;
-        int j = higher;
-        int midVal = arr[lower + (higher - lower) / 2];
-        while (i <= j) {
-            if(arr[i] < midVal)
-            {
-                System.out.println("");
-            }
-            while (arr[i] < midVal) {
-                System.out.print(arr[i] + " ");
-                i++;
-            }
-            if(arr[j] > midVal)
-            {
-                System.out.println("");
-            }
-            while (arr[j] > midVal) {
-                System.out.print(arr[j] + " ");
-                j--;
-            }
-            if (i <= j) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    private static void printBetweenArr(int[] arr, int lo, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            System.out.print(arr[i] + " ");
         }
-        if (i < higher) {
-            sort(i, higher, arr);
-        }
-        if (j > lower) {
-            sort(lower, j, arr);
-
-        }
+        System.out.println("");
     }
 }
